7d431bd332256e25bf99a023b24cf0394712a205
Amine <Mohamed.Boudribila@johnabbottcollege.net>
2023-06-19 12:47:43 -0400

    first commit

---
+++ MyProgram7.java
@@ -0,0 +1,18 @@
+package class2;
+
+import java.util.Scanner;
+
+public class MyProgram7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		Scanner kb = new Scanner(System.in);
+		if(totalMark , 50 ) {
+			grade = 'F';
+		else if (totalMark , 50 ) {
+		}
+
+}
